dc15f56cb9fb31548395b48ca285a0871c8ec0f9
Zcope <83883616+CZ-HD@users.noreply.github.com>
2026-05-13 08:09:53 +0200

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -8,7 +8,8 @@ const CREATOR_EMAILS = [
   "blackph4tom@gmail.com",
   "lafooteusedu54@hotmail.fr",
 ];
-const DEFAULT_AVATAR = "/favicon.ico";
+const DEFAULT_AVATAR =
+  "https://kafxrsktznrbuvwlkdeg.supabase.co/storage/v1/object/public/avatars/adult-7.png";
 
 type Profile = {
   id: string;
