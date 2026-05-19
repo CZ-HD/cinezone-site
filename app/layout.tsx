6934a0ea5d0651f3fa37e826e54c8cb39cc107e5
Zcope <83883616+CZ-HD@users.noreply.github.com>
2026-05-19 09:36:55 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,14 +10,30 @@ import "./globals.css";
 import NotificationsBell from "./components/NotificationsBell";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://www.cinezone-hd.fr"),
+
   title: "CineZone HD",
   description: "Films gratuits et catalogue CineZone HD",
 
-  icons: {
-  icon: "/favicon.ico",
-  shortcut: "/favicon.ico",
+ icons: {
+  icon: [
+    { url: "/favicon.ico" },
+    {
+      url: "/android-chrome-192x192.png",
+      sizes: "192x192",
+      type: "image/png",
+    },
+    {
+      url: "/android-chrome-512x512.png",
+      sizes: "512x512",
+      type: "image/png",
+    },
+  ],
+  shortcut: ["/favicon.ico"],
+  apple: ["/apple-touch-icon.png"],
 },
 
+  manifest: "/site.webmanifest",
   verification: {
     google: "iKzI4FL0MlAnHucVSprcOLlCgxowMTB2DJENXsljGpE",
   },
