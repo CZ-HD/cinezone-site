6567cc1056b5cb2d31d60b6142f4e48fbf73e7fc
Zcope <83883616+CZ-HD@users.noreply.github.com>
2026-05-22 17:54:36 +0200

    Update page.tsx

--- app/demande-film/page.tsx
+++ app/demande-film/page.tsx
@@ -575,16 +575,7 @@ export default function DemandeFilmPage() {
 
                             {isAdmin && (
                               <>
-                                <button
-                                  onClick={() => {
-                                    setReplyId(d.id);
-                                    setReplyText(d.admin_reply || "");
-                                  }}
-                                  style={btnBlue}
-                                >
-                                  💬 Répondre
-                                </button>
-                                <button
+                                
                                   onClick={() => supprimerDemande(d.id)}
                                   style={btnRed}
                                 >
