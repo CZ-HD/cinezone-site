b3f021f5b83678c538739f81d4ba406cda172a84
Zcope <83883616+CZ-HD@users.noreply.github.com>
2026-05-23 17:30:46 +0200

    Update route.ts

--- app/api/send-mail/route.ts
+++ app/api/send-mail/route.ts
@@ -11,7 +11,7 @@ export async function POST(req: Request) {
 
       to: body.to
         .split(",")
-        .map((mail) => mail.trim()),
+        .map((mail: string) => mail.trim()),
 
       subject: body.subject,
 
