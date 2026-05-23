9996a158ff83b2f11fb3c974d7638a17946cb969
Zcope <83883616+CZ-HD@users.noreply.github.com>
2026-05-23 14:33:27 +0200

    Update route.ts

--- app/api/send-mail/route.ts
+++ app/api/send-mail/route.ts
@@ -1,1 +1,26 @@
+import { Resend } from "resend";
 
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const data = await resend.emails.send({
+      from: "CineZone HD <noreply@cinezone-hd.fr>",
+      to: body.to,
+      subject: body.subject,
+      html: body.html,
+    });
+
+    return Response.json({
+      success: true,
+      data,
+    });
+  } catch (error) {
+    return Response.json({
+      success: false,
+      error,
+    });
+  }
+}
