a33d106b577bcab76a2436832badd304cb1fc0a8
Zcope <83883616+CZ-HD@users.noreply.github.com>
2026-06-04 15:39:49 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import HomeMiniChat from "@/components/HomeMiniChat";
+import HomeMiniChat from "@/app/components/HomeMiniChat";
 
 const API_KEY = "783698341437f0c7827887dbd9a2b426";
 const BASE_URL = "https://api.themoviedb.org/3";
