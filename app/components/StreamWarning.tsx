2c37d1ee2bc556864ad1f479da4928bef92b5fa2
Zcope <83883616+CZ-HD@users.noreply.github.com>
2026-06-30 19:28:09 +0200

    Update StreamWarning.tsx

--- app/components/StreamWarning.tsx
+++ app/components/StreamWarning.tsx
@@ -72,7 +72,7 @@ export default function StreamWarning({ movieId }: Props) {
       </button>
 
       {/* Fenêtre */}
-      {false && (
+      {open && (
         <div
           style={{
             position: "fixed",
