beb7540133b742fa8c6ec0807b3d717057922920
Zcope <83883616+CZ-HD@users.noreply.github.com>
2026-07-27 20:28:20 +0200

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -390,20 +390,21 @@ const result = [];
 
 for (const [movieId, count] of topIds) {
   const { data: movie } = await supabase
-  .from("downloads")
-  .select("id,title")
-  .eq("id", Number(movieId))
-  .single();
+    .from("downloads")
+    .select("id,title")
+    .eq("id", Number(movieId))
+    .single();
 
-if (!movie) {
-  console.log("Film introuvable :", movieId);
-}
+  if (!movie) {
+    console.log("Film introuvable :", movieId);
+  }
 
-if (movie) {
-  result.push({
-    ...movie,
-    count,
-  });
+  if (movie) {
+    result.push({
+      ...movie,
+      count,
+    });
+  }
 }
 
 console.log("LOGS", logs);
